021a7868bf8a6ac7e6403abecfa9023a45a6ae62
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-08-30 04:07:49 +0530

    Finalised Driver Function

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.news.models.Articles;
+import com.example.news.models.NewsClass;
+import com.example.news.networking.ApiClient;
+import com.example.news.networking.ApiInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String API_KEY = "95daef3e1ec646b6a49cad1eef6262f1";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Articles> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setNestedScrollingEnabled(false);
+
+        LoadJson();
+    }
+    public void LoadJson(){
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+        Call<NewsClass> call;
+        call = apiInterface.getNews(country,API_KEY);
+        call.enqueue(new Callback<NewsClass>() {
+            @Override
+            public void onResponse(Call<NewsClass> call, Response<NewsClass> response) {
+                if(response.isSuccessful() && response.body().getArticles()!= null){
+
+                    if(!articles.isEmpty()){
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(articles,MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }else {
+                    Toast.makeText(MainActivity.this,"No Result !!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsClass> call, Throwable t) {
+
+            }
+        });
     }
 }
